8cb39ead38ac41354c874375c7203052ed8cff75
iquerspindola <102175063+iquerspindola@users.noreply.github.com>
2024-09-27 17:16:46 -0500

    rating Plugin and configuration

--- src/main/java/com/socotra/deployment/customer/MyRatingPlugin.java
+++ src/main/java/com/socotra/deployment/customer/MyRatingPlugin.java
@@ -1,164 +1,181 @@
 package com.socotra.deployment.customer;
 
 
+import com.socotra.coremodel.RatingItem;
+import com.socotra.coremodel.RatingSet;
+import com.socotra.platform.tools.ULID;
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
 public class MyRatingPlugin implements RatePlugin {
-//    @Override
-//    public RatingSet rate(MotorhomeInsuranceQuoteRequest request) {
-//        var quote = request.quote();
-//        var duration = request.duration();
-//        List<RatingItem> ratingItems = new ArrayList<>();
-//        for( var motorHome: quote.motorhomes()){
-//            if(motorHome.motorhomeCover()!=null){
-//                ratingItems.add(rateMotorHomeCover(motorHome));
-//            }
-//            if(motorHome.contentsCover()!=null){
-//                ratingItems.add(rateContentsCover(motorHome));
-//            }
-//            if(motorHome.liabilityCover()!=null){
-//                ratingItems.add(rateLiabilityCover(motorHome));
-//            }
-//            if(motorHome.layupCover()!=null){
-//                ratingItems.add(rateLayupCover(motorHome));
-//            }
-//            if(motorHome.windscreenCover()!=null){
-//                ratingItems.add(rateWindscreenCover(motorHome));
-//            }
-//        }
-//        return RatingSet.builder().ok(true).ratingItems(ratingItems).build();
-//    }
-//    @Override
-//    public RatingSet rate(MotorhomeInsuranceRequest request) {
-//        var duration = request.duration();
-//        List<RatingItem> ratingItems = new ArrayList<>();
-//        List<MotorhomePolicy> motorHomes = new ArrayList<>();
-//        request.segment().ifPresent(s-> motorHomes.addAll(s.motorhomes()));
-//        for( var motorHome : motorHomes){
-//            if(motorHome.motorhomeCover()!=null){
-//                ratingItems.add(rateMotorHomeCover(motorHome));
-//            }
-//            if(motorHome.contentsCover()!=null){
-//                ratingItems.add(rateContentsCover(motorHome));
-//            }
-//            if(motorHome.liabilityCover()!=null){
-//                ratingItems.add(rateLiabilityCover(motorHome));
-//            }
-//            if(motorHome.layupCover()!=null){
-//                ratingItems.add(rateLayupCover(motorHome));
-//            }
-//            if(motorHome.windscreenCover()!=null){
-//                ratingItems.add(rateWindscreenCover(motorHome));
-//            }
-//        }
-//        request.segment().ifPresent(s-> ratingItems.add(rateFee(s.locator(), duration)));
-//        if (!ratingItems.isEmpty()) {
-//            request.segment().ifPresent(s-> ratingItems.add(rateTax(s.locator(), ratingItems)));
-//        }
-//        return RatingSet.builder().ok(true).ratingItems(ratingItems).build();
-//    }
-//
-//    private RatingItem rateMotorHomeCover(Motorhome motorHome) {
-//        var cover = motorHome.motorhomeCover();
-//        var rate = 0.003 * new BigDecimal(cover.data().motorhomeSumInsured()).doubleValue();
-//        rate *= switch (cover.BE()) {
-//            case BE_1000 -> 0.8;
-//            case BE_2000 -> 0.82;
-//            case BE_1500 -> 0.85;
-//            default -> 1;
-//        };
-//        return RatingItem.builder()
-//                .elementLocator(cover.locator())
-//                .chargeType(ChargeType.premium)
-//                .rate(BigDecimal.valueOf(rate))
-//                .build();
-//    }
-//
-//    private RatingItem rateContentsCover(Motorhome motorHome){
-//        var contents = motorHome.contentsCover();
-//        double rate = 30;
-//        rate *= switch (contents.CSI()) {
-//            case CSI_1000 -> 0.9;
-//            case CSI_10000 -> 0.91;
-//            case CSI_2000 -> 0.92;
-//            case CSI_20000 -> 0.95;
-//            case CSI_3000 -> 0.96;
-//            case CSI_4000 -> 0.97;
-//            case CSI_5000 -> 0.98;
-//            default -> 1;
-//        };
-//        return RatingItem.builder()
-//                .elementLocator(contents.locator())
-//                .chargeType(ChargeType.premium)
-//                .rate(BigDecimal.valueOf(rate))
-//                .build();
-//    }
-//
-//    private RatingItem rateLiabilityCover(Motorhome motorHome){
-//        double rate = 40;
-//        var liab = motorHome.liabilityCover();
-//        rate *= switch (liab.LLE()) {
-//            case LLE_1000 -> 0.9;
-//            case LLE_1500 -> 0.91;
-//            case LLE_2000 -> 0.92;
-//            default -> 1;
-//        };
-//        return RatingItem.builder()
-//                .elementLocator(liab.locator())
-//                .chargeType(ChargeType.premium)
-//                .rate(BigDecimal.valueOf(rate))
-//                .build();
-//    }
-//
-//    private RatingItem rateLayupCover(Motorhome motorHome){
-//        var layUp = motorHome.layupCover();
-//        double rate = 0.001 * 10;
-//        rate *= switch (layUp.data().months()) {
-//            case "No" -> 0.1;
-//            case "Jan" -> 0.90;
-//            case "Feb"-> 0.91;
-//            case "Mar" -> 0.92;
-//            case "Apr" -> 0.93;
-//            case "May" -> 0.94;
-//            case "Jun" -> 0.95;
-//            case "Jul" -> 0.96;
-//            case "Aug" -> 0.97;
-//            case "Sep" -> 0.98;
-//            case "Oct" -> 0.99;
-//            case "Nov" -> 0.70;
-//            case "Dec" -> 0.71;
-//            default -> 1;
-//        };
-//        return RatingItem.builder()
-//                .elementLocator(layUp.locator())
-//                .chargeType(ChargeType.premium)
-//                .rate(BigDecimal.valueOf(rate))
-//                .build();
-//    }
-//
-//    private RatingItem rateWindscreenCover(Motorhome motorHome){
-//        var windScreen = motorHome.windscreenCover();
-//        double rate = 0.001 * 40;
-//        rate *= switch (windScreen.WL()) {
-//            case WL_1000 -> 0.9;
-//            default -> 1;
-//        };
-//        return RatingItem.builder()
-//                .elementLocator(windScreen.locator())
-//                .chargeType(ChargeType.premium)
-//                .rate(BigDecimal.valueOf(rate))
-//                .build();
-//    }
-//
-//
-//
-//    private RatingItem rateTax(ULID locator, List<RatingItem> ratingItems) {
-//        BigDecimal sum = ratingItems.stream().map(RatingItem::rate).reduce(BigDecimal.ZERO, BigDecimal::add);
-//        return RatingItem.builder()
-//                .elementLocator(locator)
-//                .chargeType(ChargeType.GST)
-//                .rate(sum.multiply(BigDecimal.valueOf(0.0525)))
-//                .build();
-//    }
-//
+
+
+    @Override
+    public RatingSet rate(IndustrialSpecialPlantQuoteRequest request) {
+        var quote = request.quote();
+        var duration = request.duration();
+        List<RatingItem> ratingItems = new ArrayList<>();
+
+        if(quote.additionalBenefits() != null){
+            ratingItems.add(rateAdditionalBenefits(quote.additionalBenefits()));
+        }
+
+        if(quote.broadformLiability() != null){
+            ratingItems.add(rateBroadForm(quote.broadformLiability()));
+        }
+
+        for(var scheduleMachinery : quote.scheduleOfMachinerys()){
+            if(scheduleMachinery.breakdown() !=null){
+                ratingItems.add(rateBreakDown(scheduleMachinery));
+            }
+            if(scheduleMachinery.damage() !=null){
+                ratingItems.add(rateDamage(scheduleMachinery));
+            }
+            if(scheduleMachinery.financialProtection() !=null){
+                ratingItems.add(rateFinancialProtection(scheduleMachinery));
+            }
+            if(scheduleMachinery.hiredInPlant() !=null){
+                ratingItems.add(rateHireInPlant(scheduleMachinery));
+            }
+            if(scheduleMachinery.roadRisk() !=null){
+                ratingItems.add(rateRoadRisk(scheduleMachinery));
+            }
+        }
+
+        return RatingSet.builder().ok(true).ratingItems(ratingItems).build();
+    }
+    @Override
+    public RatingSet rate(IndustrialSpecialPlantRequest request) {
+        var duration = request.duration();
+        List<RatingItem> ratingItems = new ArrayList<>();
+        List<ScheduleOfMachinery> machineries = new ArrayList<>();
+
+        request.segment().ifPresent(segment -> {
+            if(segment.additionalBenefits() !=null) {
+                ratingItems.add(rateAdditionalBenefits(segment.additionalBenefits()));
+            }
+            if(segment.broadformLiability() != null) {
+                ratingItems.add(rateBroadForm(segment.broadformLiability()));
+            }
+
+            machineries.addAll(segment.scheduleOfMachinerys());
+
+            for(var machinery : machineries){
+                if(machinery.breakdown()!= null){
+                    ratingItems.add(rateBreakDown(machinery));
+                }
+                if(machinery.damage()!= null){
+                    ratingItems.add(rateDamage(machinery));
+                }
+                if(machinery.financialProtection()!= null){
+                    ratingItems.add(rateFinancialProtection(machinery));
+                }
+                if(machinery.hiredInPlant()!= null){
+                    ratingItems.add(rateHireInPlant(machinery));
+                }
+                if(machinery.roadRisk()!= null){
+                    ratingItems.add(rateRoadRisk(machinery));
+                }
+            }
+            //ratingItems.add(rateFee(segment.locator(), duration));
+
+            if (!ratingItems.isEmpty()) {
+                ratingItems.add(rateTax(segment.locator(), ratingItems));
+            }
+        });
+
+        return RatingSet.builder().ok(true).ratingItems(ratingItems).build();
+    }
+
+    private RatingItem rateAdditionalBenefits(AdditionalBenefits addBenefits){
+        var rate = 0.001 * new BigDecimal(addBenefits.data().totalSumInsured()).doubleValue();
+        return RatingItem.builder()
+                .elementLocator(addBenefits.locator())
+                .chargeType(ChargeType.premium)
+                .rate(BigDecimal.valueOf(rate))
+                .build();
+    }
+    private RatingItem rateBroadForm(BroadformLiability broadLiability){
+        //Broadform Liability = BroadformLimit Selection x Factor for each limit
+        var rate = .5 *
+        switch (broadLiability.broadformLimit()) {
+            case BFL_5 -> 0.8;
+            case BFL_10 -> 0.82;
+            case BFL_20 -> 0.85;
+            case BFL_50 -> 0.85;
+            default -> 1;
+        };
+        return RatingItem.builder()
+                .elementLocator(broadLiability.locator())
+                .chargeType(ChargeType.premium)
+                .rate(BigDecimal.valueOf(rate))
+                .build();
+
+    }
+    private RatingItem rateBreakDown(ScheduleOfMachinery machinery){
+        var cover = machinery.breakdown();
+        var rate = .002 * cover.data().totalSumInsured();
+        return RatingItem.builder()
+                .elementLocator(cover.locator())
+                .chargeType(ChargeType.premium)
+                .rate(BigDecimal.valueOf(rate))
+                .build();
+    }
+    private  RatingItem rateDamage(ScheduleOfMachinery machinery){
+        var cover = machinery.damage();
+        var rate = .004 * cover.data().totalSumInsured();
+        return RatingItem.builder()
+                .elementLocator(cover.locator())
+                .chargeType(ChargeType.premium)
+                .rate(BigDecimal.valueOf(rate))
+                .build();
+    }
+    private  RatingItem rateFinancialProtection(ScheduleOfMachinery machinery){
+        var cover = machinery.financialProtection();
+        var rate = .001 * new BigDecimal(cover.data().totalSumInsured()).doubleValue();
+        return RatingItem.builder()
+                .elementLocator(cover.locator())
+                .chargeType(ChargeType.premium)
+                .rate(BigDecimal.valueOf(rate))
+                .build();
+    }
+    private  RatingItem rateHireInPlant(ScheduleOfMachinery machinery){
+        var cover = machinery.hiredInPlant();
+        var rate = .001 * cover.data().totalSumInsured();
+        return RatingItem.builder()
+                .elementLocator(cover.locator())
+                .chargeType(ChargeType.premium)
+                .rate(BigDecimal.valueOf(rate))
+                .build();
+    }
+    private  RatingItem rateRoadRisk(ScheduleOfMachinery machinery){
+        var cover = machinery.roadRisk();
+        var rate = .2 *
+                switch (cover.roadriskLimit()) {
+                    case RRL_20 -> 0.8;
+                    case RRL_30 -> 0.82;
+                    default -> 1;
+                };
+
+        return RatingItem.builder()
+                .elementLocator(cover.locator())
+                .chargeType(ChargeType.premium)
+                .rate(BigDecimal.valueOf(rate))
+                .build();
+    }
+
+
+
+    private RatingItem rateTax(ULID locator, List<RatingItem> ratingItems) {
+        BigDecimal sum = ratingItems.stream().map(RatingItem::rate).reduce(BigDecimal.ZERO, BigDecimal::add);
+        return RatingItem.builder()
+                .elementLocator(locator)
+                .chargeType(ChargeType.GST)
+                .rate(sum.multiply(BigDecimal.valueOf(0.0525)))
+                .build();
+    }
+
 //    private RatingItem rateFee(ULID locator, BigDecimal duration) {
 //        return RatingItem.builder()
 //                .elementLocator(locator)
--- src/main/java/com/socotra/deployment/customer/MyValidationPlugin.java
+++ src/main/java/com/socotra/deployment/customer/MyValidationPlugin.java
@@ -3,6 +3,7 @@ package com.socotra.deployment.customer;
 import com.socotra.coremodel.ValidationItem;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import java.util.Collections;
 
 public class MyValidationPlugin implements ValidationPlugin{
     private static final Logger log = LoggerFactory.getLogger(MyValidationPlugin.class);
@@ -10,6 +11,13 @@ public class MyValidationPlugin implements ValidationPlugin{
     public ValidationItem validate(IndustrialSpecialPlantQuoteRequest request) {
         log.info("Validation Plugin");
         var validator = ValidationItem.builder();
-        return validator.build();
+        var fields =  Collections.<String>emptyList();
+        //Validate schedule Of Machinery items
+        for (var entry : request.quote().scheduleOfMachinerys()) {
+            if(entry.data().machine().isEmpty()){
+                fields.add(entry.locator() + " - Has empty items");
+            }
+        }
+        return validator.addErrors(fields).build();
     }
 }
